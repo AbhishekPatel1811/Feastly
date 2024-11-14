28ede00d99185c8495c6654849e2a65e8c23c9ff
Abhishekpatel1811 <patelabhishek1811@gmail.com>
2024-11-14 18:24:30 +0530

    Updated logic for useRestaurantData

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -10,21 +10,23 @@ const RestaurantCard = ({
   sla,
 }) => {
   return (
-    <div className="w-[20vw] px-2 py-4">
-      <img
-        className="w-full h-[30vh] object-cover object-bottom rounded-xl drop-shadow-xl"
-        src={IMG_CDN_URL + cloudinaryImageId}
-      />
-      <h3 className="mt-4 text-lg font-bold">{name}</h3>
-      <div className="mt-2 flex justify-start items-center gap-2 font-semibold">
-        <img className="h-4" src={stars} alt="stars" />
-        <h4 className="font-thin tracking-wide">
-          {avgRating} • {sla.slaString}
-        </h4>
+    <>
+      <div className="w-[20vw] px-2 py-4">
+        <img
+          className="w-full h-[30vh] object-cover object-bottom rounded-xl drop-shadow-xl"
+          src={IMG_CDN_URL + cloudinaryImageId}
+        />
+        <h3 className="mt-4 text-lg font-bold">{name}</h3>
+        <div className="mt-2 flex justify-start items-center gap-2 font-semibold">
+          <img className="h-4" src={stars} alt="stars" />
+          <h4 className="font-thin tracking-wide">
+            {avgRating} • {sla.slaString}
+          </h4>
+        </div>
+        <h4 className="mt-2 text-sm font-normal">{cuisines.join(", ")}</h4>
+        <h4 className="mt-2 text-sm font-normal">{areaName}</h4>
       </div>
-      <h4 className="mt-2 text-sm font-normal">{cuisines.join(", ")}</h4>
-      <h4 className="mt-2 text-sm font-normal">{areaName}</h4>
-    </div>
+    </>
   );
 };
 
--- src/utils/useRestaurantData.js
+++ src/utils/useRestaurantData.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 const useRestaurantData = () => {
   const [allRestaurants, setAllRestaurants] = useState([]); //restaurantList to be used if to show hardcoded data
   const [filteredRestaurants, setFilteredRestaurants] = useState([]); //restaurantList to be used if to show hardcoded data
+  const [topRestaurantTitle, setTopRestaurantTitle] = useState();
 
   //Get the data from API
   useEffect(() => {
@@ -17,28 +18,28 @@ const useRestaurantData = () => {
 
       console.log("Json data:", json);
 
-      const restaurants = extractRestaurantsData(json);
-      if (restaurants) {
-        setAllRestaurants(restaurants);
-        setFilteredRestaurants(restaurants);
-      }
+      const cards = json?.data?.cards || [];
+
+      //Top 20 restaurants
+      setTopRestaurantTitle(cards[1]?.card?.card?.header?.title);
+      setAllRestaurants(
+        cards[1].card?.card?.gridElements?.infoWithStyle?.restaurants
+      );
+      setFilteredRestaurants(
+        cards[1].card?.card?.gridElements?.infoWithStyle?.restaurants
+      );
     } catch (error) {
       console.log("Error", error);
     }
   }
 
-  const extractRestaurantsData = (json) => {
-    const cards = json?.data?.cards || [];
-    for (let card of cards) {
-      const restaurantsData =
-        card?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-      if (Array.isArray(restaurantsData)) return restaurantsData;
-    }
-    return null;
-  };
-
   //Return restaurant Data
-  return { allRestaurants, filteredRestaurants, setFilteredRestaurants };
+  return {
+    allRestaurants,
+    filteredRestaurants,
+    setFilteredRestaurants,
+    topRestaurantTitle,
+  };
 };
 
 export default useRestaurantData;
